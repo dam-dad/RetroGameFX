8678b9c4748fea85f5501c7d607af48e0d569867
DagadezDGH <davidgarridohdez@gmail.com>
2021-01-31 20:20:02 +0000

    Muchas cositas xd

--- src/test/java/base/App.java
+++ src/test/java/base/App.java
@@ -1,15 +1,23 @@
 package base;
 
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
 import controllersTest.MainViewController;
 import controllersTest.OpcionesViewController;
 import controllersTest.SelectViewController;
+import javafx.animation.Transition;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.scene.media.*;
 
 public class App extends Application{
-
+	private static HiloMusica hiloMusica;
+	private static double volumenMusica = 0.3;
 	private MainViewController controller;
 	private SelectViewController SelectViewController;
 	private OpcionesViewController OpcionesViewController;
@@ -33,7 +41,7 @@ public class App extends Application{
 		primaryStage.show();
 		primaryStage.setResizable(false);
 		//primaryStage.getIcons().add(new Image("/images/classroom-24x24.png"));
-		
+		playMusica("MainSong.mp3");
 	}
 	public static void main(String[] args) {
 		launch(args);
@@ -47,4 +55,39 @@ public class App extends Application{
 	public static void CambiarAOpciones(){
 		primaryStage.setScene(opcionesScene);
 	}
+	public static double getVolumenMusica() {
+		return volumenMusica;
+	}
+	public static void setVolumenMusica(double volumenMusic) {
+		App.volumenMusica = volumenMusic;
+	}
+	
+	public static void playMusica(String file) {
+		hiloMusica = new HiloMusica(file);
+		hiloMusica.run();
+	}
+	
 }
+class HiloMusica extends Thread{
+	String file;
+	MediaPlayer player;
+	
+	public HiloMusica(String file) {
+		this.file =file;
+		
+	}
+	public void play() {
+	URL path= getClass().getResource(file);
+	Media media;
+	try {
+		media= new Media(path.toURI().toString());
+		player=new MediaPlayer(media);
+		player.setVolume(App.getVolumenMusica());
+		player.setCycleCount(Transition.INDEFINITE);
+		player.play();
+	}catch(URISyntaxException e) {
+		e.printStackTrace();
+		
+	}
+	}
+	}
