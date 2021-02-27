c9270050c3dea878a27e937f149172ad9f58c0f2
Cosme <carcafes@gmail.com>
2021-02-27 20:20:08 +0000

    revision brickbreaker
    creacion de un metodo de reconocimiento de caras impactables y colision de estos con las pelotas

--- src/main/java/dad/javafx/retrogamefx/games/brickbreaker/BrickBreaker.java
+++ src/main/java/dad/javafx/retrogamefx/games/brickbreaker/BrickBreaker.java
@@ -1,6 +1,7 @@
 package dad.javafx.retrogamefx.games.brickbreaker;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
@@ -17,10 +18,20 @@ import dad.javafx.retrogamefx.games.pong.Wall;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
+
+
 public class BrickBreaker extends GameScene{
 	
 	private static final double BALL_R = 20;
 	private boolean gameStarted;
+	static int maxFilas = 10;
+	static int maxColumnas = 20;
+	private static final int width = 800;
+	private static final int height = 600;
+	static int anchoBloque = width / maxColumnas;
+	static int altoBloque = height / maxFilas;
+	ArrayList<Cara> Caras;
+	Bricks[][] Bloques;
 	
 	// model
 	private Player player;
@@ -47,12 +58,13 @@ public class BrickBreaker extends GameScene{
     private Label player1LivesLabel;
 	
 	public BrickBreaker() {
-		super("/fxml/BrickBreaker.fxml", 800, 600);
+		super("/fxml/BrickBreaker.fxml", width, height);
 	}
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
-
+		Bloques = new Bricks[maxFilas + 2][maxColumnas + 2];
+		 Caras = new ArrayList();
 		background = new Background(Color.BLACK);
 		background.setBounds(0, 0, getWidth(), getHeight());
 		
@@ -148,4 +160,105 @@ private void update(double diff) {
 			}
 		}
 	}
+//------------------------Colisiones by cosme
+
+
+
+
+public void update(Bricks[][] bloques) {
+	for (int fila = 1; fila <= maxFilas - 2; fila++) {
+		for (int columna = 1; columna <= maxColumnas - 2; columna++) {
+			esNull(bloques[fila][columna],fila,columna);
+		}
+	}
+	colision(Caras,ball);
+}
+
+public void esNull(Bricks bloque,int fila, int columna) {
+	if (izquierda(fila, columna)) {
+		Caras.add(new Cara(bloque.getX(),bloque.getY(), bloque.getWidth(), bloque.getHeight(), true, fila, columna));
+	}
+	if (derecha(fila, columna)) {
+		Caras.add(new Cara(bloque.getX() + bloque.getHeight(),bloque.getY(), bloque.getWidth(), bloque.getHeight(), true, fila, columna));
+	}
+	if (arriba(fila, columna)) {
+		Caras.add(new Cara(bloque.getX(),bloque.getY(), bloque.getWidth(), bloque.getHeight(), false, fila, columna));
+	}
+	if (abajo(fila, columna)) {
+		Caras.add(new Cara(bloque.getX(),bloque.getY()+bloque.getHeight(), bloque.getWidth(), bloque.getHeight(), false, fila, columna));
+	}
+
+}
+
+// comprueba si tiene encima un bloque normal o protegido
+public boolean izquierda(int fila, int columna) {
+	if (Bloques[fila][columna - 1] == null)
+		return true;
+	else
+		return false;
+}
+
+// comprueba si tiene debajo un bloque normal o protegido
+public boolean derecha(int fila, int columna) {
+	if (Bloques[fila][columna + 1] == null)
+		return true;
+	else
+		return false;
+}
+
+// comprueba si tiene a la izquierda un bloque normal o protegido
+public boolean arriba(int fila, int columna) {
+	if (Bloques[fila - 1][columna] == null)
+		return true;
+	else
+		return false;
+}
+
+// comprueba si tiene a la derecha un bloque normal o protegido
+public boolean abajo(int fila, int columna) {
+	if (Bloques[fila + 1][columna] == null)
+		return true;
+	else
+		return false;
+}
+
+public void colision(ArrayList<Cara> caras, Ball ball) {
+	if (recorrerCaras(caras, ball)[0] != -1) {
+		Bloques[recorrerCaras(caras, ball)[0]][recorrerCaras(caras, ball)[1]] = null;
+		player.setScore(player.getScore()+1);
+	}
+}
+
+public int[] recorrerCaras(ArrayList<Cara> caras, Ball ball) {
+	int[] aux = { -1, -1 };
+	for (Cara x : caras) {
+		// añadir colisiones de otros 3 puntos de la pelota
+		if (comparadorCaraPunto(x, ball)) {
+			aux[0] = x.fila;
+			aux[1] = x.columna;
+		}
+	}
+	return aux;
+}
+
+//colision de 1 punto de la pelota hace falta poner 4 puntos a la pelota
+public boolean comparadorCaraPunto(Cara cara, Ball ball) {
+	boolean aux = false;
+	if (cara.d) {
+		// linea horizontal misma Y, contiene X
+		double X = cara.x, Y = cara.y, W = cara.w, Xb = ball.getX(), Yb = ball.getY();
+		if (Y == Yb & X >= Xb & Xb >= (X + W)) {
+			aux = true;
+		}
+	} else {
+		// linea vertical misma X, contiene Y
+		double X = cara.x, Y = cara.y, H = cara.h, Xb = ball.getX(), Yb = ball.getY();
+		if (X == Xb & Y >= Yb & Yb >= (Y + H)) {
+			aux = true;
+		}
+
+	}
+	return aux;
+
+}
 }
---
+++ src/main/java/dad/javafx/retrogamefx/games/brickbreaker/Cara.java
@@ -0,0 +1,16 @@
+package dad.javafx.retrogamefx.games.brickbreaker;
+
+public class Cara {
+ public double x,y,w,h;
+ public boolean d;
+ public int fila,columna;
+ public Cara(double X,double Y,double W,double H,boolean D,int Fila,int Columna) {
+	 x=X;
+	 y=Y;
+	 w=W;
+	 h=H;
+	 d=D;
+	 fila=Fila;
+	 columna=Columna;
+ }
+}
